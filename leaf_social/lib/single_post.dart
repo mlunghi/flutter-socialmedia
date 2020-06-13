2e06cda27838847f6e1217567ca9eec3d018c19a
Matteo Lunghi <lunghi.matteo2000@gmail.com>
2020-06-13 12:33:04 +0200

    added image to single post page

--- leaf_social/lib/single_post.dart
+++ leaf_social/lib/single_post.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'demo_values.dart';
 
 class SinglePost extends StatelessWidget {
 
   const SinglePost({Key key}) : super(key: key);
 
   @override
+  @override
   Widget build(BuildContext context) {
-    return Text("Hello");
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Leaf"),
+      ),
+      body: Image.asset(DemoValues.postImage),
+    );
   }
 
 }
